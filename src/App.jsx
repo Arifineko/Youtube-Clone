06d570bbcd6f42cff1103f136245a095d0f0a28c
arifin <aeko86869@gmail.com>
2026-03-18 13:20:02 +0700

    add: video details and iframe

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,26 @@ import './App.css'
 import Home from './pages/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import NotFound from './pages/NotFound'
+import Watch from './pages/Watch'
+import { useState } from 'react'
+import { MenuContext } from './context/MenuContext'
 
 
 
 function App() {
+  const [menu, setMenu] = useState(false)
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <MenuContext.Provider value={{ menu, setMenu }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/watch' element={<Watch />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </MenuContext.Provider>
     </>
   )
 }

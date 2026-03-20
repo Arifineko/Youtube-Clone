038391ead17830828e78e02671cc130a2a102024
arifin <aeko86869@gmail.com>
2026-03-20 21:03:37 +0700

    add: loading skeleton for content page

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,18 @@
 import VideoCard from './VideoCard/VideoCard'
 import { useTrendingVideos } from '../hooks/useYoutube'
+import SkeletonCard from './SkeletonCard'
 
 const Content = () => {
     const { data, isLoading, isError } = useTrendingVideos();
 
-    if (isLoading) return <div className="pt-24 pl-20 italic">Loading...</div>
+    if (isLoading) return (
+        <div className='md:pl-20 mb-20 md:mb-0 pt-23 md:pt-30 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-rows-1 gap-4 gap-y-6 md:p-5'>
+            {[...Array(12)].map((_, i) => (
+                <SkeletonCard key={i} />
+            ))}
+        </div>
+    )
+
     if (isError) return <div className="pt-24 pl-20 text-red-500">Error loading videos.</div>
 
     return (

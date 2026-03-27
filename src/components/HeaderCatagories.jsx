eb8debdf021218937ce9ea9ae9e26718895896db
arifin <aeko86869@gmail.com>
2026-03-27 13:24:31 +0700

    fix: margin header catagories

--- src/components/HeaderCatagories.jsx
+++ src/components/HeaderCatagories.jsx
@@ -5,7 +5,7 @@ const HeaderCatagories = () => {
     const [active, setActive] = useState(catagoriesData[0]?.name || "");
 
     return (
-        <div className="flex bg-white fixed left-0 md:left-16 top-[46px] md:top-[53px] right-0 px-3 py-2 z-40 gap-3 overflow-x-auto">
+        <div className="flex bg-white fixed left-0 md:left-16 top-[46px] md:top-[60px] right-0 px-3 py-2 z-40 gap-3 overflow-x-auto">
             {catagoriesData.map((category, key) => {
                 const isActive = active === category.name;
                 return (
